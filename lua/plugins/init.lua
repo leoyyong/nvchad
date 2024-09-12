4e8b61e4d892a14fd83728a5a497531b6bc4e4b4
leo.yu <leo.yu@choiceform.com>
2024-09-12 10:48:27 +0800

    ✨ feat: 添加gopher.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,19 @@ return {{
         require("configs.dap")
     end
 }, {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    -- branch = "develop", -- if you want develop branch
+    -- keep in mind, it might break everything
+    dependencies = {"nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", "mfussenegger/nvim-dap" -- (optional) only if you use `gopher.dap`
+    },
+    -- (optional) will update plugin's deps on every update
+    build = function()
+        vim.cmd.GoInstallDeps()
+    end,
+    ---@type gopher.Config
+    opts = {}
+}, {
     "williamboman/mason-lspconfig.nvim",
     event = "VeryLazy",
     dependencies = {"nvim-lspconfig"},
